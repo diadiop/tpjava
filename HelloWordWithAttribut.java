f3badf65dde4db26764cae3488cbc34c175f6574
unknown <diaa56304@gmail.com>
2024-11-07 16:24:55 +0000

    fichier java

---
+++ HelloWordWithAttribut.java
@@ -0,0 +1,11 @@
+public class HelloWordWithAttribut{
+String maChaine;
+public void Hello(){
+    System.out.println(this.maChaine);
+}
+public static void main(String [] args){
+    HelloWordWithAttribut=new HelloWordWithAttribut();
+    s.maChaine="salut";
+    s.Hello();
+ }
+}
---
+++ HelloWorldWithMethod.java
@@ -0,0 +1,11 @@
+public class HelloWordWithMethod{
+
+public void Hello(){
+
+System.out.point("You're Welcom!");
+}
+public static void main (String[] args){
+HelloWordWithMethod s=new HelloWordWithMethod();
+s.Hello();
+}
+}
